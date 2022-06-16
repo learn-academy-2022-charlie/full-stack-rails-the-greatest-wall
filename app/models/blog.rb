c4a6b4a57e6df9968fee229eeca7ccd07b427ba2
CJOcode <cjoconnor24@gmail.com>
2022-06-16 11:58:34 -0700

    rpec complete

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ApplicationRecord
-    
+    validates :title, :content, presence:true
+    validates :title, uniqueness:true
+    validates :title, length:{minimum: 10}
 end
---
+++ spec/models/blog_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Blog, type: :model do
+  it 'has a title that cannot be empty' do
+   new_blog = Blog.create title:"", content:"kdjvsdfvkdfbvdfkfvfkvfkj"
+    expect(new_blog.errors[:title]).to_not be_empty
+  end
+  it 'has content that cannot be empty' do
+    new_blog = Blog.create title:"My first blog", content:""
+    expect(new_blog.errors[:content]).to_not be_empty
+  end
+  it 'has a title that is unique' do
+    Blog.create(title:"My first blog", content:"kdjvsdfvkdfbvdfkfvfkvfkj")
+    new_blog = Blog.create title:"My first blog", content:"kdjvsdfvkdfbvdfkfvfkvfkj"
+    expect(new_blog.errors[:title]).to_not be_empty
+  end
+  it 'has a title that is ten characters long' do
+    new_blog = Blog.create title:"My first", content:"kdjvsdfvkdfbvdfkfvfkvfkj"
+    expect(new_blog.errors[:title]).to_not be_empty
+  end
+end
+
